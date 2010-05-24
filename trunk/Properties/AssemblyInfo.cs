08b4ad89fb6c2ba6b23dd69fd3e9299170e26a66
omri.spector <omri.spector@19dd5b36-5fe6-5d33-1ae0-45b3b378dcf7>
2010-05-24 21:48:20 +0000

    filled in assembly info

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Guitar")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Google Unit Test Application Runner")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Omri Spector")]
 [assembly: AssemblyProduct("Guitar")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
