c8e6805a9f2d49d280ac85d89b709e06e67ff69b
vertexwahn@gmail.com <vertexwahn@gmail.com@19dd5b36-5fe6-5d33-1ae0-45b3b378dcf7>
2012-04-30 13:16:34 +0000

    Textbox for startup folder now scales with window resize events

--- trunk/GuitarForm.Designer.cs
+++ trunk/GuitarForm.Designer.cs
@@ -64,21 +64,21 @@
             // 
             // errorScreen
             // 
-            this.errorScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.errorScreen.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.errorScreen.Location = new System.Drawing.Point(3, 0);
             this.errorScreen.Multiline = true;
             this.errorScreen.Name = "errorScreen";
-            this.errorScreen.Size = new System.Drawing.Size(520, 174);
+            this.errorScreen.Size = new System.Drawing.Size(497, 174);
             this.errorScreen.TabIndex = 9;
             // 
             // numFailuresLabel
             // 
             this.numFailuresLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.numFailuresLabel.AutoSize = true;
-            this.numFailuresLabel.Location = new System.Drawing.Point(783, 186);
+            this.numFailuresLabel.Location = new System.Drawing.Point(748, 186);
             this.numFailuresLabel.Name = "numFailuresLabel";
             this.numFailuresLabel.Size = new System.Drawing.Size(13, 13);
             this.numFailuresLabel.TabIndex = 5;
@@ -97,7 +97,7 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(679, 186);
+            this.label2.Location = new System.Drawing.Point(644, 186);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 4;
@@ -115,7 +115,7 @@
             // goBtn
             // 
             this.goBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.goBtn.Location = new System.Drawing.Point(372, 382);
+            this.goBtn.Location = new System.Drawing.Point(354, 382);
             this.goBtn.Name = "goBtn";
             this.goBtn.Size = new System.Drawing.Size(75, 23);
             this.goBtn.TabIndex = 7;
@@ -125,12 +125,12 @@
             // 
             // lineLabel
             // 
-            this.lineLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lineLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.lineLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lineLabel.Location = new System.Drawing.Point(9, 150);
             this.lineLabel.Name = "lineLabel";
-            this.lineLabel.Size = new System.Drawing.Size(787, 23);
+            this.lineLabel.Size = new System.Drawing.Size(752, 23);
             this.lineLabel.TabIndex = 2;
             // 
             // label3
@@ -145,7 +145,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(731, 4);
+            this.button1.Location = new System.Drawing.Point(696, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -155,35 +155,35 @@
             // 
             // failureListBox
             // 
-            this.failureListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.failureListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.failureListBox.FormattingEnabled = true;
             this.failureListBox.Location = new System.Drawing.Point(0, 0);
             this.failureListBox.Name = "failureListBox";
-            this.failureListBox.Size = new System.Drawing.Size(251, 173);
+            this.failureListBox.Size = new System.Drawing.Size(239, 173);
             this.failureListBox.TabIndex = 8;
             this.failureListBox.SelectedIndexChanged += new System.EventHandler(this.failureListBox_SelectedIndexChanged);
             // 
             // exeFilename
             // 
-            this.exeFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.exeFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.exeFilename.FormattingEnabled = true;
             this.exeFilename.Location = new System.Drawing.Point(125, 6);
             this.exeFilename.Name = "exeFilename";
-            this.exeFilename.Size = new System.Drawing.Size(600, 21);
+            this.exeFilename.Size = new System.Drawing.Size(565, 21);
             this.exeFilename.TabIndex = 0;
             this.exeFilename.TextChanged += new System.EventHandler(this.exeFilename_TextChanged);
             // 
             // clParams
             // 
-            this.clParams.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clParams.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.clParams.FormattingEnabled = true;
             this.clParams.Location = new System.Drawing.Point(125, 33);
             this.clParams.Name = "clParams";
-            this.clParams.Size = new System.Drawing.Size(395, 21);
+            this.clParams.Size = new System.Drawing.Size(360, 21);
             this.clParams.TabIndex = 2;
             // 
             // label4
@@ -201,7 +201,7 @@
             this.shouldShuffle.AutoSize = true;
             this.shouldShuffle.Checked = true;
             this.shouldShuffle.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.shouldShuffle.Location = new System.Drawing.Point(537, 36);
+            this.shouldShuffle.Location = new System.Drawing.Point(502, 36);
             this.shouldShuffle.Name = "shouldShuffle";
             this.shouldShuffle.Size = new System.Drawing.Size(59, 17);
             this.shouldShuffle.TabIndex = 3;
@@ -212,7 +212,7 @@
             // 
             this.shouldRunDisabled.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.shouldRunDisabled.AutoSize = true;
-            this.shouldRunDisabled.Location = new System.Drawing.Point(602, 37);
+            this.shouldRunDisabled.Location = new System.Drawing.Point(567, 37);
             this.shouldRunDisabled.Name = "shouldRunDisabled";
             this.shouldRunDisabled.Size = new System.Drawing.Size(119, 17);
             this.shouldRunDisabled.TabIndex = 4;
@@ -230,12 +230,12 @@
             // 
             // filter
             // 
-            this.filter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.filter.FormattingEnabled = true;
             this.filter.Location = new System.Drawing.Point(125, 85);
             this.filter.Name = "filter";
-            this.filter.Size = new System.Drawing.Size(600, 21);
+            this.filter.Size = new System.Drawing.Size(565, 21);
             this.filter.TabIndex = 6;
             // 
             // hideConsole
@@ -244,7 +244,7 @@
             this.hideConsole.AutoSize = true;
             this.hideConsole.Checked = true;
             this.hideConsole.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.hideConsole.Location = new System.Drawing.Point(719, 37);
+            this.hideConsole.Location = new System.Drawing.Point(684, 37);
             this.hideConsole.Name = "hideConsole";
             this.hideConsole.Size = new System.Drawing.Size(89, 17);
             this.hideConsole.TabIndex = 5;
@@ -253,9 +253,9 @@
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.splitContainer1.Location = new System.Drawing.Point(12, 203);
             this.splitContainer1.Name = "splitContainer1";
             // 
@@ -266,8 +266,8 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.errorScreen);
-            this.splitContainer1.Size = new System.Drawing.Size(784, 179);
-            this.splitContainer1.SplitterDistance = 254;
+            this.splitContainer1.Size = new System.Drawing.Size(749, 179);
+            this.splitContainer1.SplitterDistance = 242;
             this.splitContainer1.TabIndex = 17;
             // 
             // label6
@@ -281,21 +281,23 @@
             // 
             // textBoxStartupFolder
             // 
+            this.textBoxStartupFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxStartupFolder.Location = new System.Drawing.Point(125, 59);
             this.textBoxStartupFolder.Name = "textBoxStartupFolder";
-            this.textBoxStartupFolder.Size = new System.Drawing.Size(599, 20);
+            this.textBoxStartupFolder.Size = new System.Drawing.Size(564, 20);
             this.textBoxStartupFolder.TabIndex = 19;
             // 
             // progressBar
             // 
-            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.progressBar.Location = new System.Drawing.Point(9, 118);
             this.progressBar.Maximum = 9;
             this.progressBar.Minimum = 0;
             this.progressBar.Name = "progressBar";
             this.progressBar.ProgressBarColor = System.Drawing.Color.Blue;
-            this.progressBar.Size = new System.Drawing.Size(787, 24);
+            this.progressBar.Size = new System.Drawing.Size(752, 24);
             this.progressBar.TabIndex = 3;
             this.progressBar.Value = 0;
             // 
@@ -303,7 +305,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(820, 409);
+            this.ClientSize = new System.Drawing.Size(785, 409);
             this.Controls.Add(this.textBoxStartupFolder);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.splitContainer1);
